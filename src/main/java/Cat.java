207e07cedb0fadebe0abaaa1475ee19e32f4b1a7
Belf <sleven182@mail.ru>
2021-05-28 15:23:36 +0300

    add task6

---
+++ src/main/java/Cat.java
@@ -0,0 +1,39 @@
+import java.util.Objects;
+
+public class Cat {
+    private int id;
+    private final int age;
+    private final String name;
+
+    public Cat(int id, int age, String name) {
+        this.id = id;
+        this.age = age;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    @Override
+    public String toString() {
+        return "Cat{" +
+                "id=" + id +
+                ", age=" + age +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Cat cat = (Cat) o;
+        return age == cat.age && Objects.equals(name, cat.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(age, name);
+    }
+}
---
+++ src/main/java/Node.java
@@ -0,0 +1,12 @@
+public class Node {
+    public Cat cat;
+    public Node leftChild;
+    public Node rightChild;
+
+    @Override
+    public String toString() {
+        return "Node{" +
+                "cat=" + cat +
+                '}';
+    }
+}
